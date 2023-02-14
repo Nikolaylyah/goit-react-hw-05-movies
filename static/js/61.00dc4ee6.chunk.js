e9630c29eb7c9977d8e021563ea7f401373d1149
Nikolaylyah <107050313+Nikolaylyah@users.noreply.github.com>
2023-02-14 12:21:17 +0000

    Deploying to gh-pages from @ Nikolaylyah/goit-react-hw-05-movies@c674d21417cb79654232e8cf73bc80f91f52b671 🚀

---
+++ static/js/61.00dc4ee6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[61],{3061:function(e,n,r){r.r(n),r.d(n,{default:function(){return o}});var t=r(1087),i=r(184);function o(){return(0,i.jsxs)("div",{children:[(0,i.jsx)("h1",{children:" 404 Sorry, the page was not found"}),(0,i.jsx)(t.rU,{to:"/",children:"To main page"})]})}}}]);
+//# sourceMappingURL=61.00dc4ee6.chunk.js.map
